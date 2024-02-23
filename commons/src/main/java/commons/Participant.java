0695054a11e8c42427354d699b7fa6314aa1e406
Stefan-Tudor Lupsan <t.lupsan@student.tudelft.nl>
2024-02-23 11:46:57 +0100

    Added content to Participant class.

--- commons/src/main/java/commons/Participant.java
+++ commons/src/main/java/commons/Participant.java
@@ -1,4 +1,52 @@
 package commons;
 
+import java.util.Objects;
+
 public class Participant {
+    private int uuid;
+    private String name;
+    private String email;
+
+    public Participant(int uuid, String name, String email) {
+        this.uuid = uuid;
+        this.name = name;
+        this.email = email;
+    }
+
+    public int getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(int uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participant that = (Participant) o;
+        return getUuid() == that.getUuid() && Objects.equals(getName(), that.getName()) && Objects.equals(getEmail(), that.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUuid(), getName(), getEmail());
+    }
 }
