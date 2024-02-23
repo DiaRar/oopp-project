386171cc899b92d473061b13177cc3ff49632c02
Jerzy Karremans <jkarremans@tudelft.nl>
2024-02-23 09:07:03 +0100

    Added equals and hashcode + comments

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -3,10 +3,13 @@ package commons;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-
 import java.util.ArrayList;
+import java.util.Objects;
 import java.util.UUID;
 
+/**
+ * Represents an event in the database.
+ */
 public class Event {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -16,6 +19,14 @@ public class Event {
     private ArrayList<Expense> expenses;
     private ArrayList<Tag> tags;
 
+    /**
+     * Constructs an Event object with specified name, UUID, expenses, and tags.
+     *
+     * @param name     The name of the event.
+     * @param uuid     The UUID of the event.
+     * @param expenses The list of expenses associated with the event.
+     * @param tags     The list of tags associated with the event.
+     */
     public Event(String name, UUID uuid, ArrayList<Expense> expenses, ArrayList<Tag> tags) {
         this.name = name;
         this.uuid = uuid;
@@ -23,10 +34,20 @@ public class Event {
         this.tags = tags;
     }
 
+    /**
+     * Constructs an Event object with specified name and UUID.
+     *
+     * @param name The name of the event.
+     * @param uuid The UUID of the event.
+     */
     public Event(String name, UUID uuid) {
         this.name = name;
         this.uuid = uuid;
     }
+
+    /**
+     * Constructs an empty Event object.
+     */
     public Event() {}
 
     public String getName() {
@@ -59,5 +80,31 @@ public class Event {
 
     public void setTags(ArrayList<Tag> tags) {
         this.tags = tags;
+    }
+
+    /**
+     * Indicates whether some other object is equal to this one.
+     * Two events are considered equal if they have the same ID, name, UUID, expenses, and tags.
+     *
+     * @param o The reference object with which to compare.
+     * @return true if this object is the same as the o argument; false otherwise.
+     */
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Event event = (Event) o;
+        return id == event.id && Objects.equals(name, event.name) && Objects.equals(uuid, event.uuid) && Objects.equals(expenses, event.expenses) && Objects.equals(tags, event.tags);
+    }
+
+    /**
+     * Returns a hash code value for the object.
+     * The hash code is calculated based on the ID, name, UUID, expenses, and tags of the event.
+     *
+     * @return A hash code value for this object.
+     */
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, uuid, expenses, tags);
     }
 }
