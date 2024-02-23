bd044f1eab36136a8b726d828991ddc41a23d629
Stefan-Tudor Lupsan <t.lupsan@student.tudelft.nl>
2024-02-23 11:46:57 +0100

    Added content to Participant class. Amend: separated name property into firstName and lastName, added protected no arg constructor for the object mapper.

--- commons/src/main/java/commons/Participant.java
+++ commons/src/main/java/commons/Participant.java
@@ -1,4 +1,70 @@
 package commons;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+import java.util.Objects;
+
+@Entity
 public class Participant {
+    @Id
+    private int uuid;
+    private String firstName;
+    private String lastName;
+    private String email;
+
+    //for object mapper
+    protected Participant() {
+
+    }
+    public Participant(int uuid, String firstName, String lastName, String email) {
+        this.uuid = uuid;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.email = email;
+    }
+
+    public int getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(int uuid) {
+        this.uuid = uuid;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Participant that = (Participant) o;
+        return getUuid() == that.getUuid() && Objects.equals(getFirstName(), that.getFirstName()) && Objects.equals(getLastName(), that.getLastName()) && Objects.equals(getEmail(), that.getEmail());
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(getUuid(), getFirstName(), getLastName(), getEmail());
+    }
 }
