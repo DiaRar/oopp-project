1782a1cf9687867b9177c144c17cc8b5e80ac34b
Alessandro Neri <a.neri@student.tudelft.nl>
2024-02-23 12:15:42 +0100

    Added attributes, constructors, getters and setters to BankAccount class

--- commons/src/main/java/commons/BankAccount.java
+++ commons/src/main/java/commons/BankAccount.java
@@ -1,4 +1,47 @@
 package commons;
 
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+import java.util.UUID;
+
 public class BankAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+    private String iban;
+    private String bic;
+    private UUID uuid;
+
+    public BankAccount(String iban, String bic) {
+        this.iban = iban;
+        this.bic = bic;
+    }
+
+    public BankAccount() {}
+
+    public String getIban() {
+        return iban;
+    }
+
+    public void setIban(String iban) {
+        this.iban = iban;
+    }
+
+    public String getBic() {
+        return bic;
+    }
+
+    public void setBic(String bic) {
+        this.bic = bic;
+    }
+
+    public UUID getUuid() {
+        return uuid;
+    }
+
+    public void setUuid(UUID uuid) {
+        this.uuid = uuid;
+    }
 }
