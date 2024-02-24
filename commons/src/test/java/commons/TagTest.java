b047222a34bea6915ff2a387d6b78a3fc6c93a71
Blago Gunev <B.I.Gunev-1@student.tudelft.nl>
2024-02-24 17:22:09 +0100

    Make tests for Tag class

--- commons/src/test/java/commons/TagTest.java
+++ commons/src/test/java/commons/TagTest.java
@@ -9,9 +9,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class TagTest {
 
+    private Tag tag1, tag2, tag3;
+
     @BeforeEach
     public void beforeEach() {
-
+        tag1 = new Tag("food", new Color(255, 0, 0));
+        tag2 = new Tag("food1", new Color(255, 0, 255));
+        tag3 = new Tag("food", new Color(255, 0, 0));
     }
 
     @Test
@@ -27,22 +31,28 @@ class TagTest {
 
     @Test
     public void equalsTest() {
-
+        // Same tags should be equal
+        assertEquals(tag1, tag3);
     }
 
     @Test
     public void notEqualsTest() {
-        
+        // Different tag name and color tags should not be equal
+        assertNotEquals(tag1, tag2);
     }
 
     @Test
     public void equalsHashCodeTest() {
-
+        // Equal tags should have the same hashCode
+        assertEquals(tag1.hashCode(), tag3.hashCode());
     }
 
     @Test
     public void toStringTest() {
-
+        // String should contain the tag name
+        assertTrue(tag1.toString().contains("food"));
+        // String should contain RGB values
+        assertTrue(tag1.toString().contains("255"));
     }
 
 }
