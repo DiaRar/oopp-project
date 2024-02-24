96565870580cc758cdd2c6a4ae7ff9dd5d68a4e7
Blago Gunev <B.I.Gunev-1@student.tudelft.nl>
2024-02-24 15:38:23 +0100

    Create skeleton for Tag class

--- commons/src/main/java/commons/Tag.java
+++ commons/src/main/java/commons/Tag.java
@@ -1,15 +1,73 @@
 package commons;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
+import java.awt.*;
+import java.util.ArrayList;
 import java.util.UUID;
 
 @Entity
 public class Tag {
+    // Attributes
     @Id
     @GeneratedValue(strategy = GenerationType.UUID)
     private UUID id;
+    private String name;
+    private Color color;
+    // Relations
+    @ManyToOne
+    private Event event;
+    @ManyToMany
+    private ArrayList<Expense> expenses;
+
+    public Tag() {
+    }
+
+    public Tag(String name, Color color) {
+        this.name = name;
+        this.color = color;
+    }
+
+    public Tag(String name, Color color, Event event, ArrayList<Expense> expenses) {
+        this.name = name;
+        this.color = color;
+        this.event = event;
+        this.expenses = expenses;
+    }
+
+    public UUID getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Event getEvent() {
+        return event;
+    }
+
+    public void setEvent(Event event) {
+        this.event = event;
+    }
+
+    public ArrayList<Expense> getExpenses() {
+        return expenses;
+    }
+
+    public void setExpenses(ArrayList<Expense> expenses) {
+        this.expenses = expenses;
+    }
 }
