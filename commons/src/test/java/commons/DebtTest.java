1145d860f2d871b3fbae6c63a60fd91b63ba4032
Cristian Turcan <C.Turcan@student.tudelft.nl>
2024-02-25 14:30:24 +0100

    Resolved some checkstyle warnings

--- commons/src/test/java/commons/DebtTest.java
+++ commons/src/test/java/commons/DebtTest.java
@@ -11,10 +11,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class DebtTest {
     @Test
-    public void constructorTest(){
+    public void constructorTest() {
         LocalDateTime date = LocalDateTime.of(2024, 2, 23, 23, 33);
         Debt debt = new Debt(new ImmutablePair<>(2.0, Currency.getInstance(Locale.US)), "Hello",
-                date, new Participant("a","b","c"), new Participant("a","d","b"));
+                date, new Participant("a", "b", "c"), new Participant("a", "d", "b"));
 
         assertTrue(date.equals(debt.date()));
         assertTrue(debt.lender().getFirstName().equals("a"));
@@ -25,16 +25,16 @@ public class DebtTest {
         LocalDateTime date = LocalDateTime.of(2024, 2, 23, 23, 33);
 
         Debt debt = new Debt(new ImmutablePair<>(2.0, Currency.getInstance(Locale.US)), "Hello",
-                date, new Participant("a","b","c"), new Participant("a","d","b"));
+                date, new Participant("a", "b", "c"), new Participant("a", "d", "b"));
 
         Debt debt1 = new Debt(new ImmutablePair<>(3.0, Currency.getInstance(Locale.US)), "Hello",
-                date, new Participant("a","b","c"), new Participant("a","d","b"));
+                date, new Participant("a", "b", "c"), new Participant("a", "d", "b"));
         // Two different debts should not be equal
         assertNotEquals(debt, debt1);
 
         // Two debts with the same attributes should be equal
         Debt debt2 = new Debt(new ImmutablePair<>(2.0, Currency.getInstance(Locale.US)), "Hello",
-                date, new Participant("a","b","c"), new Participant("a","d","b"));
+                date, new Participant("a", "b", "c"), new Participant("a", "d", "b"));
         assertEquals(debt, debt2);
     }
 
@@ -43,17 +43,17 @@ public class DebtTest {
         LocalDateTime date = LocalDateTime.of(2024, 2, 23, 23, 33);
 
         Debt debt = new Debt(new ImmutablePair<>(2.0, Currency.getInstance(Locale.US)), "Hello",
-                date, new Participant("a","b","c"), new Participant("a","d","b"));
+                date, new Participant("a", "b", "c"), new Participant("a", "d", "b"));
 
         Debt debt1 = new Debt(new ImmutablePair<>(3.0, Currency.getInstance(Locale.US)), "Hello",
-                date, new Participant("a","b","c"), new Participant("a","d","b"));
+                date, new Participant("a", "b", "c"), new Participant("a", "d", "b"));
 
         // Two different debts should not have an equal hashcode
         assertNotEquals(debt.hashCode(), debt1.hashCode());
 
         // Two debts with the same attributes should have the same hashcode
         Debt debt2 = new Debt(new ImmutablePair<>(2.0, Currency.getInstance(Locale.US)), "Hello",
-                date, new Participant("a","b","c"), new Participant("a","d","b"));
+                date, new Participant("a", "b", "c"), new Participant("a", "d", "b"));
         assertEquals(debt.hashCode(), debt2.hashCode());
     }
 
@@ -62,7 +62,7 @@ public class DebtTest {
         LocalDateTime date = LocalDateTime.of(2024, 2, 23, 23, 33);
 
         Debt debt = new Debt(new ImmutablePair<>(2.0, Currency.getInstance(Locale.US)), "Hello",
-                date, new Participant("a","b","c"), new Participant("a","d","b"));
+                date, new Participant("a", "b", "c"), new Participant("a", "d", "b"));
 
         // ToString method should contain the name of the debtor
         assertTrue(debt.toString().contains("a"));
