56beb5f0448856875de216f59a6dfae3a0de9ae5
Blago Gunev <B.I.Gunev-1@student.tudelft.nl>
2024-03-02 13:40:16 +0100

    Add template code for all api paths

--- server/src/main/java/server/api/ExpensesController.java
+++ server/src/main/java/server/api/ExpensesController.java
@@ -3,7 +3,7 @@ package server.api;
 import commons.Expense;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import server.database.ExpenseRepository;
+import server.database.EventRepository;
 
 import java.util.Collection;
 import java.util.UUID;
@@ -12,19 +12,37 @@ import java.util.UUID;
 @RequestMapping("/api/event/{id}/expenses")
 public class ExpensesController {
 
-    private final ExpenseRepository repo;
+    private final EventRepository eventRepo;
 
-    public ExpensesController(ExpenseRepository repo) {
-        this.repo = repo;
+    public ExpensesController(EventRepository eventRepo) {
+        this.eventRepo = eventRepo;
     }
 
     @GetMapping(path = {"", "/"})
-    public Collection<Expense> getExpenses(@PathVariable("id") UUID eventId) {
-        return null;
+    public ResponseEntity<Collection<Expense>> getExpenses(@PathVariable("id") UUID eventId) {
+        return ResponseEntity.ok(eventRepo.findById(eventId).get().getExpenses());
     }
 
     @PostMapping(path = {"", "/"})
-    public ResponseEntity<Expense> postExpense(@RequestBody Expense expense) {
+    public ResponseEntity<Expense> postExpense(@PathVariable("id") UUID eventId, @RequestBody Expense expense) {
+        return null;
+    }
+
+    @GetMapping("/{expense_id}")
+    public ResponseEntity<Expense>
+        getExpenseById(@PathVariable("id") UUID eventId, @PathVariable("expense_id") UUID expenseId) {
+        return null;
+    }
+
+    @PutMapping("/{expense_id}")
+    public ResponseEntity<Expense>
+        putExpense(@PathVariable("id") UUID eventId, @PathVariable("expense_id") UUID expenseId) {
+        return null;
+    }
+
+    @DeleteMapping("/{expense_id}")
+    public ResponseEntity<Expense>
+        deleteExpense(@PathVariable("id") UUID eventId, @PathVariable("expense_id") UUID expenseId) {
         return null;
     }
 }
