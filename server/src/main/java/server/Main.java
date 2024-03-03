67763e12b164d75025288015aa8719a36f49450c
Ștefan Lupşan <lupsan@student.tudelft.nl>
2024-03-03 21:47:30 +0100

    Refactored API using servies

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -19,7 +19,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 
-@SpringBootApplication(scanBasePackages = {"services"})
+@SpringBootApplication
 @EntityScan(basePackages = { "commons", "server" })
 public class Main {
 
