1943e92b7b5817a7d56737dc8aed834d637972da
Alessandro Neri <a.neri@student.tudelft.nl>
2024-03-03 15:45:53 +0100

    Adjusted fxml attributes + removed CheckBox and replaced with RadioButton

--- client/src/main/java/client/scenes/AddExpenseCtrl.java
+++ client/src/main/java/client/scenes/AddExpenseCtrl.java
@@ -1,41 +1,57 @@
 package client.scenes;
 
 import client.utils.ServerUtils;
+import commons.Participant;
+import commons.Tag;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.input.KeyEvent;
+import java.util.Currency;
 
 public class AddExpenseCtrl {
 
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
+    public ToggleGroup split;
     @FXML
-    private ComboBox payer;
+    private ComboBox<String> payer;
     @FXML
     private TextField description;
     @FXML
     private TextField amount;
     @FXML
-    private ComboBox currency;
+    private ComboBox<Currency> currency;
     @FXML
     private DatePicker date;
     @FXML
-    private ComboBox tag;
+    private ComboBox<Tag> tag;
     @FXML
-    private CheckBox equallySplit;
+    private RadioButton equallySplit;
     @FXML
-    private CheckBox partialSplit;
+    private RadioButton partialSplit;
     @FXML
-    private TableView debtors;
+    private TableView<Participant> debtors;
 
     public AddExpenseCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
     }
 
-    public void ok() {}
+    public void cancel() {
+        clearFields();
+    }
 
-    public void cancel() {}
+    public void clearFields() {
+        payer.getSelectionModel().clearSelection();
+        description.clear();
+        amount.clear();
+        currency.getSelectionModel().clearSelection();
+        date.cancelEdit();
+        tag.getSelectionModel().clearSelection();
+        equallySplit.setSelected(false);
+        partialSplit.setSelected(false);
+    }
+    public void ok() {}
 
     public void keyPressed(KeyEvent e) {
         switch (e.getCode()) {
