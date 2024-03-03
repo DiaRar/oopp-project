a5421e4426636447638ba67e57816bb1b0ed66c3
jerzy karremans <jkarremans@tudelft.nl>
2024-03-03 19:14:49 +0100

    Added a start screen

---
+++ client/src/main/java/client/scenes/StartCtrl.java
@@ -0,0 +1,19 @@
+package client.scenes;
+
+public class StartCtrl {
+
+    /**
+     * Displays the create event screen.
+     */
+    public void create(){
+        System.out.println("create event screen");
+    }
+
+    /**
+     * Loads event data from the database and switches to the overview screen.
+     */
+    public void join(){
+        System.out.println("load event from database");
+        System.out.println("switch to overview");
+    }
+}
