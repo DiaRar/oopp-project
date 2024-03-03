7cdea16e726853d4c0cee0f7f6cd6d6367c709bc
Alessandro Neri <a.neri@student.tudelft.nl>
2024-03-03 18:20:31 +0100

    Added initialize method with mock data + radioButton event

--- client/src/main/java/client/scenes/AddExpenseCtrl.java
+++ client/src/main/java/client/scenes/AddExpenseCtrl.java
@@ -1,15 +1,23 @@
 package client.scenes;
 
 import client.utils.ServerUtils;
+import com.google.inject.Inject;
 import commons.Participant;
 import commons.Tag;
+import javafx.collections.FXCollections;
 import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
 import javafx.scene.control.*;
+import javafx.scene.control.TextField;
 import javafx.scene.input.KeyEvent;
+
+import java.awt.*;
+import java.net.URL;
 import java.util.Currency;
+import java.util.Locale;
+import java.util.ResourceBundle;
 
-public class AddExpenseCtrl {
-
+public class AddExpenseCtrl implements Initializable {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
     public ToggleGroup split;
@@ -32,6 +40,7 @@ public class AddExpenseCtrl {
     @FXML
     private TableView<Participant> debtors;
 
+    @Inject
     public AddExpenseCtrl(ServerUtils server, MainCtrl mainCtrl) {
         this.server = server;
         this.mainCtrl = mainCtrl;
@@ -39,6 +48,7 @@ public class AddExpenseCtrl {
 
     public void cancel() {
         clearFields();
+        mainCtrl.showOverview();
     }
 
     public void clearFields() {
@@ -46,12 +56,20 @@ public class AddExpenseCtrl {
         description.clear();
         amount.clear();
         currency.getSelectionModel().clearSelection();
-        date.cancelEdit();
+        date.setValue(null);
         tag.getSelectionModel().clearSelection();
         equallySplit.setSelected(false);
         partialSplit.setSelected(false);
     }
     public void ok() {}
+
+    public void showDebtors() {
+        if (partialSplit.isSelected()) {
+            debtors.setVisible(true);
+        } else {
+            debtors.setVisible(false);
+        }
+    }
 
     public void keyPressed(KeyEvent e) {
         switch (e.getCode()) {
@@ -64,5 +82,18 @@ public class AddExpenseCtrl {
             default:
                 break;
         }
+    }
+
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        payer.setItems(FXCollections.observableArrayList("Paul", "Mike", "Irene", "Julia"));
+        tag.setItems(FXCollections.observableArrayList(new Tag("Food", Color.orange),
+                new Tag("Ticket", Color.GREEN),
+                new Tag("Transport", Color.BLUE)));
+        currency.setItems(FXCollections.observableArrayList(Currency.getInstance(Locale.US), Currency.getInstance(Locale.UK)));
+        debtors.setItems(FXCollections.observableArrayList(new Participant("Paul", "Son", "hisEmail"),
+            new Participant("Mike", "Hawk", "hisEmail"),
+            new Participant("Irene", "Cool", "herEmail"),
+            new Participant("Julia", "Cook", "herEmail")));
     }
 }
