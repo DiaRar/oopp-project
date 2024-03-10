0164dbd8edb2289f59f5b7bb04cf9fd8208b85fe
jerzy <j.r.karremans@student.tudelft.nl>
2024-03-10 23:08:15 +0100

    added remove recent by uuid and write recent to ConfigUtils

--- client/src/main/java/client/utils/ConfigUtils.java
+++ client/src/main/java/client/utils/ConfigUtils.java
@@ -7,11 +7,12 @@ import java.io.*;
 import java.util.*;
 
 public class ConfigUtils {
-    private Reader recentsFile;
+    private File recentsFile;
     private Reader participantsFile;
 
     public ArrayList<Event> readRecents() {
-        try (BufferedReader reader = new BufferedReader(recentsFile)) {
+        createFileIfNotExists();
+        try (BufferedReader reader = new BufferedReader(new FileReader(recentsFile))) {
             ArrayList<Event> events = new ArrayList<>();
             String line;
             while ((line = reader.readLine()) != null) {
@@ -24,6 +25,43 @@ public class ConfigUtils {
             return events;
         }
         catch (IOException e) {
+            //TODO Log error and handle
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void addRecent(Event event) {
+        createFileIfNotExists();
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(recentsFile, true))) {
+            writer.newLine();
+            writer.write(event.getName() + "," + event.getId().toString());
+        }
+        catch (Exception e) {
+            //TODO Log error and handle
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void removeRecent(UUID uuidToRemove) {
+        ArrayList<Event> events = readRecents();
+        events.removeIf(event -> event.getId().equals(uuidToRemove));
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter(recentsFile))) {
+            for (Event event : events) {
+                writer.write(event.getName() + "," + event.getId().toString());
+                writer.newLine();
+            }
+        } catch (Exception e) {
+            //TODO Log error and handle
+            throw new RuntimeException(e);
+        }
+    }
+
+    private void createFileIfNotExists() {
+        try {
+            if (!recentsFile.exists()) {
+                recentsFile.createNewFile();
+            }
+        } catch (IOException e) {
             //TODO Log error and handle
             throw new RuntimeException(e);
         }
@@ -60,7 +98,7 @@ public class ConfigUtils {
         }
     }
 
-    public void setRecentsFile(Reader recentsFile) {
+    public void setRecentsFile(File recentsFile) {
         this.recentsFile = recentsFile;
     }
 
