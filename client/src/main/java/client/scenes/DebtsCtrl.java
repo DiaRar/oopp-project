1b8445cd1b698e97fef6879070500b43de9c3208
Alessandro Neri <a.neri@student.tudelft.nl>
2024-03-21 10:42:14 +0100

    finished method to create a BorderPane out of a Debt object

--- client/src/main/java/client/scenes/DebtsCtrl.java
+++ client/src/main/java/client/scenes/DebtsCtrl.java
@@ -7,9 +7,13 @@ import commons.Debt;
 import commons.Event;
 import commons.Participant;
 import javafx.fxml.FXML;
-import javafx.geometry.*;
-import javafx.scene.control.*;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import javafx.scene.text.Text;
@@ -24,8 +28,9 @@ public class DebtsCtrl {
     private final ServerUtils server;
     private final MainCtrl mainCtrl;
     private static final double DEBT_AMOUNT = 100;
-    private static final double INNER_MARGIN = 10;
-    private static final double TOP_MARGIN = 10;
+    private static final double IMAGE_SIZE = 20;
+    private static final double BORDER_PANE_MARGIN = 10;
+    private static final double FLOW_PANE_MARGIN = 5;
     private static final Font ARIAL_BOLD = new Font("Arial Bold", 12);
     @FXML
     private Button returnButton;
@@ -44,37 +49,49 @@ public class DebtsCtrl {
         // TODO instead of the hardcoded data we should use the debts of the current event with server.getDebts(event)
         List<Debt> debts = mockData();
         List<BorderPane> collection = debts.stream().map(this::debtComponent).toList();
-        debtsList.getChildren().clear();
+        // debtsList.getChildren().clear();
         debtsList.getChildren().addAll(collection);
     }
 
     public BorderPane debtComponent(Debt debt) {
         BorderPane borderPane = new BorderPane();
+
+        // Icon of the bank (on the left side)
+        ImageView bankIcon = new ImageView("../../resources/main/client/images/bank.png");
+        bankIcon.setFitHeight(IMAGE_SIZE);
+        bankIcon.setFitWidth(IMAGE_SIZE);
+        BorderPane.setMargin(bankIcon, new Insets(0, 0, 0, BORDER_PANE_MARGIN));
+        // TODO add onClick event for the image (add bank details on the bottom of the the BorderPane
+
+        // Description of the debt (in the center)
+        HBox description = new HBox();
         TextFlow content = new TextFlow();
-        VBox vbox = new VBox();
-        BorderPane.setMargin(vbox, new Insets(TOP_MARGIN, INNER_MARGIN, TOP_MARGIN, INNER_MARGIN));
-
-        Button settled = new Button();
-        settled.setText("Settle");
-
-        Button reminder = new Button();
-        reminder.setText("Remind");
-
         Text debtor = new Text(debt.getDebtor().getNickname());
         Text amount = new Text(debt.getAmount().toString().concat("EUR"));
         Text payer = new Text(debt.getPayer().getNickname());
         debtor.setFont(ARIAL_BOLD);
         payer.setFont(ARIAL_BOLD);
         content.getChildren().addAll(debtor, new Text(" owes "), amount, new Text(" to "), payer);
-        vbox.getChildren().addAll(content);
+        description.getChildren().addAll(content);
+        BorderPane.setMargin(description, new Insets(BORDER_PANE_MARGIN, BORDER_PANE_MARGIN, BORDER_PANE_MARGIN, BORDER_PANE_MARGIN));
 
-        borderPane.setLeft(vbox);
-        borderPane.setCenter(reminder);
-        borderPane.setRight(settled);
+        // Buttons on the right side
+        HBox buttons = new HBox();
+        Button reminder = new Button();
+        reminder.setText("Remind");
+        Button settled = new Button();
+        settled.setText("Settle");
+        buttons.getChildren().addAll(reminder, settled);
+        HBox.setMargin(settled, new Insets(FLOW_PANE_MARGIN, FLOW_PANE_MARGIN, FLOW_PANE_MARGIN, 0));
+        HBox.setMargin(reminder, new Insets(FLOW_PANE_MARGIN, FLOW_PANE_MARGIN, FLOW_PANE_MARGIN, 0));
 
-        BorderPane.setAlignment(vbox, Pos.CENTER_LEFT);
-        BorderPane.setAlignment(reminder, Pos.CENTER);
-        BorderPane.setAlignment(settled, Pos.CENTER);
+        borderPane.setLeft(bankIcon);
+        borderPane.setCenter(description);
+        borderPane.setRight(buttons);
+
+        BorderPane.setAlignment(bankIcon, Pos.CENTER_LEFT);
+        BorderPane.setAlignment(description, Pos.CENTER_LEFT);
+        BorderPane.setAlignment(buttons, Pos.CENTER_RIGHT);
 
         return borderPane;
     }
@@ -98,7 +115,7 @@ public class DebtsCtrl {
     public List<Debt> mockData() {
         Event event = new Event("New Year Party");
         Participant p1 = new Participant("Ale", "email1");
-        Participant p2 = new Participant("Becky", "email2");
+        Participant p2 = new Participant("Very Long Name to see what happens", "email2");
         Participant p3 = new Participant("Cactus", "email3");
         Participant p4 = new Participant("Lazarus", "email4");
         List<Debt> list = new ArrayList<>();
