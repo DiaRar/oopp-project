a88ffc54a09a40c2afe88b1588f16f221124dc57
jerzy <j.r.karremans@student.tudelft.nl>
2024-04-08 15:52:32 +0200

    opening explorer on export

--- admin/src/main/java/admin/scenes/OverviewCtrl.java
+++ admin/src/main/java/admin/scenes/OverviewCtrl.java
@@ -4,6 +4,7 @@ import admin.uicomponents.RemoveButtonCell;
 import admin.utils.Config;
 import admin.utils.ServerUtils;
 import com.google.inject.Inject;
+import commons.Debt;
 import commons.Event;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
@@ -11,6 +12,7 @@ import javafx.fxml.FXML;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
 
+import java.awt.*;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -92,16 +94,20 @@ public class OverviewCtrl {
 
         try (FileWriter fileWriter = new FileWriter(file)) {
             fileWriter.write(json);
+            if (Desktop.isDesktopSupported() && Desktop.getDesktop().isSupported(Desktop.Action.OPEN)) {
+                Desktop.getDesktop().open(new File(config.getJsonPath()));
+        }
         } catch (IOException e) {
             throw new RuntimeException(e);
         }
-
-        //TODO open explorer or notify the user where the file has been sorted
     }
 
     public void upload() {
-        System.out.println("upload");
+        System.out.println("importing");
+        serverUtils.importDatabase("{\"bankAccounts\":[{\"iban\":\"232\",\"bic\":\"434\"},{\"iban\":\"what eban\",\"bic\":\"bick\"},{\"iban\":\"second iban\",\"bic\":\"second bic\"}],\"events\":[{\"id\":\"c71d356a-c514-4ac1-b264-e2bae44efda2\",\"name\":\"yes\",\"creationDate\":[2024,4,7,19,14,46,982485000],\"lastActivityDate\":[2024,4,7,19,14,46,982485000]},{\"id\":\"b11adf9a-cd54-4505-8f43-3279d96d09cf\",\"name\":\"new event\",\"creationDate\":[2024,4,7,19,58,52,516895000],\"lastActivityDate\":[2024,4,7,19,58,52,516895000]}],\"debts\":[{\"debtorId\":\"94dc882d-0867-4fe1-81aa-f3064fd4494c\",\"payerId\":\"94dc882d-0867-4fe1-81aa-f3064fd4494c\",\"amount\":0.0,\"eventId\":\"c71d356a-c514-4ac1-b264-e2bae44efda2\"},{\"debtorId\":\"afcf4e2b-b2be-4125-abe6-674a973c9bfa\",\"payerId\":\"afcf4e2b-b2be-4125-abe6-674a973c9bfa\",\"amount\":0.0,\"eventId\":\"b11adf9a-cd54-4505-8f43-3279d96d09cf\"},{\"debtorId\":\"2c085050-b525-4e58-9cf4-f0182bea138c\",\"payerId\":\"afcf4e2b-b2be-4125-abe6-674a973c9bfa\",\"amount\":-15.0,\"eventId\":\"b11adf9a-cd54-4505-8f43-3279d96d09cf\"},{\"debtorId\":\"afcf4e2b-b2be-4125-abe6-674a973c9bfa\",\"payerId\":\"2c085050-b525-4e58-9cf4-f0182bea138c\",\"amount\":15.0,\"eventId\":\"b11adf9a-cd54-4505-8f43-3279d96d09cf\"}],\"expenses\":[{\"id\":\"22659b2e-7253-4f44-99a4-d24924044622\",\"amount\":34.0,\"title\":\"fdw\",\"date\":[2024,4,3,0,0],\"eventId\":\"c71d356a-c514-4ac1-b264-e2bae44efda2\",\"payerId\":\"94dc882d-0867-4fe1-81aa-f3064fd4494c\"},{\"id\":\"4c9ecf43-6d57-4d66-bda2-0a06d68ce6a3\",\"amount\":30.0,\"title\":\"beans\",\"date\":[2024,4,3,0,0],\"eventId\":\"b11adf9a-cd54-4505-8f43-3279d96d09cf\",\"payerId\":\"afcf4e2b-b2be-4125-abe6-674a973c9bfa\"}],\"participants\":[{\"id\":\"94dc882d-0867-4fe1-81aa-f3064fd4494c\",\"email\":\"dsajkda\",\"nickname\":\"hello \",\"bankIBAN\":\"232\",\"eventId\":\"c71d356a-c514-4ac1-b264-e2bae44efda2\"},{\"id\":\"afcf4e2b-b2be-4125-abe6-674a973c9bfa\",\"email\":\"jon email\",\"nickname\":\"johnavan\",\"bankIBAN\":\"what eban\",\"eventId\":\"b11adf9a-cd54-4505-8f43-3279d96d09cf\"},{\"id\":\"2c085050-b525-4e58-9cf4-f0182bea138c\",\"email\":\"second email\",\"nickname\":\"second johnavan\",\"bankIBAN\":\"second iban\",\"eventId\":\"b11adf9a-cd54-4505-8f43-3279d96d09cf\"}],\"tags\":[]}\n");
+
     }
+
 
     public void addEvent(Event event) {
         tableView.getItems().add(event);
