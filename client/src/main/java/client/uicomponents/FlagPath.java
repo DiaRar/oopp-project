f7eeadb8392bb379972afae4d1a065c4a198c5e0
Blago Gunev <B.I.Gunev-1@student.tudelft.nl>
2024-04-13 13:38:12 +0200

    Fix checkstyle

--- client/src/main/java/client/uicomponents/FlagPath.java
+++ client/src/main/java/client/uicomponents/FlagPath.java
@@ -1,6 +1,6 @@
 package client.uicomponents;
 
 public abstract class FlagPath {
-    public static String UK_FLAG_PATH = "client/flags/UK.png";
-    public static String NL_FLAG_PATH = "client/flags/Netherlands.png";
+    public static final String UK_FLAG_PATH = "client/flags/UK.png";
+    public static final String NL_FLAG_PATH = "client/flags/Netherlands.png";
 }
